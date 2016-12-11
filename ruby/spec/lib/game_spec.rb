b2e27430f689681481682a6af88f40ddbc1da180
Ariel Spear <simpleredfox@gmail.com>
2016-12-11 01:42:12 -0800

    listed rules to be tested

--- ruby/spec/lib/game_spec.rb
+++ ruby/spec/lib/game_spec.rb
@@ -5,3 +5,9 @@ describe Game do
   it 'exists' do
   end
 end
+
+# rules to test:
+# Any cell that is alive, remains alive in the next generation with two or three neighbors.
+# Any cell with three living neighbors becomes alive in the next generation, as if by reproduction.
+# Any cell with fewer than two living neighbors dies, as if by starvation.
+# Any cell with more than three living neighbors dies, as if by overpopulation.
